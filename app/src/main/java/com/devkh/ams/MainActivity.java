40dae7585f857cae66dc9645a4c87427f370bacd
Developers Cambodia <devkh@MacBooks-MacBook-Pro.local>
2021-11-25 15:52:02 +0700

    first commit

---
+++ app/src/main/java/com/devkh/ams/MainActivity.java
@@ -0,0 +1,15 @@
+package com.devkh.ams;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
